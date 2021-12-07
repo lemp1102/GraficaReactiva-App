4a517fd467174b4c9fb6f68995c4775c794aadc3
Lowis McLin <lemp9002@yandex.com>
2021-12-07 04:01:09 -0500

    Technical Test
    Grafica Reactiva

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-spacer />
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      title: 'Gráfica'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,156 @@
+<template>
+  <v-row justify='center' align='center'>
+    <v-col sm='8' md='6'>
+      <v-select
+        v-model='periodo'
+        :items="items"
+        label="Período"
+        outlined
+        @input='dataSetChage'>
+      </v-select>
+      <BarChart :chart-data='chartData' :options='barChartOptions' />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: ["Primer Semestre", "Segundo Semestre"],
+      barChartData: [
+        {
+          labels: [
+            "Enero",
+            "Febrero",
+            "Marzo",
+            "Abril",
+            "Mayo",
+            "Junio",
+          ],
+          datasets: [
+            {
+              label: "Visualizaciones",
+              data: [2, 1, 16, 3, 4, 5],
+              backgroundColor: "rgba(20, 255, 0, 0.3)",
+              borderColor: "rgba(100, 255, 0, 1)",
+              borderWidth: 2,
+            },
+          ],
+        },
+        {
+          labels: [
+            "Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre",
+          ],
+          datasets: [
+            {
+              label: "Visualizaciones",
+              data: [0, 0, 4, 12, 2, 15],
+              backgroundColor: "rgba(20, 255, 0, 0.3)",
+              borderColor: "rgba(100, 255, 0, 1)",
+              borderWidth: 2,
+            },
+          ],
+        },
+      ],
+      barChartOptions: {
+        responsive: true,
+        legend: {
+          display: false,
+        },
+        title: {
+          display: true,
+          text: "Google analytics data",
+          fontSize: 24,
+          fontColor: "#6b7280",
+        },
+        tooltips: {
+          backgroundColor: "#17BF62",
+        },
+        scales: {
+          xAxes: [
+            {
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                max: 7,
+                min: 0,
+                stepSize: 1,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+        },
+      },
+      chartData: '',
+      periodo: ''
+    };
+  },
+  mounted() {
+    this.chartData= this.barChartData[0];
+  },
+  methods: {
+    dataSetChage(){
+      switch (this.periodo) {
+        case "Primer Semestre":   this.chartData =
+          {
+            labels: [
+              "Enero",
+              "Febrero",
+              "Marzo",
+              "Abril",
+              "Mayo",
+              "Junio",
+            ],
+            datasets: [
+              {
+                label: "Visualizaciones",
+                data: [2, 1, 16, 3, 4, 5],
+                backgroundColor: "rgba(20, 255, 0, 0.3)",
+                borderColor: "rgba(100, 255, 0, 1)",
+                borderWidth: 2,
+              },
+            ],
+          };
+                                  console.log(this.chartData);
+                                  break;
+        case "Segundo Semestre":  this.chartData = {
+          labels: [
+            "Julio",
+            "Agosto",
+            "Septiembre",
+            "Octubre",
+            "Noviembre",
+            "Diciembre",
+          ],
+          datasets: [
+            {
+              label: "Visualizaciones",
+              data: [0, 0, 4, 12, 2, 15],
+              backgroundColor: "rgba(20, 255, 0, 0.3)",
+              borderColor: "rgba(100, 255, 0, 1)",
+              borderWidth: 2,
+            },
+          ],
+        };
+                                  console.log(this.chartData);
+                                  break;
+      }
+    }
+  },
+
+};
+</script>
